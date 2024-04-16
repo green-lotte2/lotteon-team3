30a092e94a021e3605545af5ace62b6c0dee60a0
heega <m_mny1004@naver.com>
2024-04-17 02:44:03 +0900

    회원가입 - 유효성 검사 전

---
+++ src/main/java/kr/co/lotteon/service/member/MemberService.java
@@ -0,0 +1,28 @@
+package kr.co.lotteon.service.member;
+
+
+import kr.co.lotteon.dto.member.MemberDTO;
+import kr.co.lotteon.entity.member.Member;
+import kr.co.lotteon.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MemberService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+    private final MemberRepository memberRepository;
+    
+    public void save(MemberDTO memberDTO){
+        // 비밀번호 암호화
+        memberDTO.setPass1(passwordEncoder.encode(memberDTO.getPass1())); 
+        Member member = modelMapper.map(memberDTO, Member.class);
+        memberRepository.save(member); 
+    }
+}
