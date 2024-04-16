8aa784f7191b7ebdc9bcdb41b3f21a254068e99d
123rhkddms2 <123rhkddms2@gmail.com>
2024-04-16 16:33:35 +0900

    add

--- src/main/java/kr/co/lotteon/repository/cs/CsRepository.java
+++ src/main/java/kr/co/lotteon/repository/cs/CsRepository.java
@@ -4,9 +4,11 @@ import kr.co.lotteon.entity.cs.BoardEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CsRepository extends JpaRepository<BoardEntity, Integer> {
 
     // 리스트 출력시(그룹, 카테고리로 구분)
+    @Query("SELECT b FROM BoardEntity b WHERE b.group = :group AND (b.cate= :cate OR :cate = 'null')")
     public Page<BoardEntity> findByGroupAndCate(String group, String cate, Pageable pageable);
 }
