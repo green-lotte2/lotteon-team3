7efe90920f5b98918e9ed3b6ca31ef5f1d018976
heega <m_mny1004@naver.com>
2024-04-16 10:36:20 +0900

    member 레이아웃 및 타임리프 적용

---
+++ src/main/java/kr/co/lotteon/controller/CompanyController.java
@@ -0,0 +1,37 @@
+package kr.co.lotteon.controller;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Log4j2
+@Controller
+public class CompanyController {
+
+    @GetMapping(value = {"/company", "/company/introduce"})
+    public String index() {
+      return ("/company/introduce");
+    }
+
+    @GetMapping(value = {"/company/notice"})
+    public String notice() {
+        return ("/company/notice");
+    }
+
+    @GetMapping(value = { "/company/promote"})
+    public String promote() {
+        return ("/company/promote");
+    }
+
+    @GetMapping(value = { "/company/manage"})
+    public String manage() {
+        return ("/company/manage");
+    }
+
+
+
+}
+
+
