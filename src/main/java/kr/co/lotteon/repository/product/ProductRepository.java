d6998507bae1dbbf2758469845201f8989e0f2be
chlvldzmf <chlvldzmf@gmail.com>
2024-04-16 20:10:55 +0900

    product Impl

--- src/main/java/kr/co/lotteon/repository/product/ProductRepository.java
+++ src/main/java/kr/co/lotteon/repository/product/ProductRepository.java
@@ -1,7 +1,8 @@
 package kr.co.lotteon.repository.product;
 
 import kr.co.lotteon.entity.product.Product;
+import kr.co.lotteon.repository.custom.ProductRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> ,ProductRepositoryCustom {
 }
