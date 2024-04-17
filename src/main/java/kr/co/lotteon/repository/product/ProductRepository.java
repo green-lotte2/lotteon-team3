b98c1c3f3fdcbdc1a02f47d5e4ef07d307419ec9
sun-gwang <kkj80911@gmail.com>
2024-04-17 11:52:33 +0900

    add

--- src/main/java/kr/co/lotteon/repository/product/ProductRepository.java
+++ src/main/java/kr/co/lotteon/repository/product/ProductRepository.java
@@ -2,9 +2,14 @@ package kr.co.lotteon.repository.product;
 
 import kr.co.lotteon.entity.product.Product;
 import kr.co.lotteon.repository.custom.ProductRepositoryCustom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> ,ProductRepositoryCustom {
+    
+    // 상품 목록 조회
+    public Page<Product> findByCate1AndCate2(int cate1, int cate2, Pageable pageable);
 }
