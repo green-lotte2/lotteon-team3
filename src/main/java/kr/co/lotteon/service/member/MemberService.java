6d2362eb2a026093a7b25c7f025be757ab0755f4
heega <m_mny1004@naver.com>
2024-04-17 17:19:03 +0900

    회원가입 유효성 검사 및 중복검사 - 이메일 인증코드 오류

--- src/main/java/kr/co/lotteon/service/member/MemberService.java
+++ src/main/java/kr/co/lotteon/service/member/MemberService.java
@@ -1,14 +1,23 @@
 package kr.co.lotteon.service.member;
 
 
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpSession;
 import kr.co.lotteon.dto.member.MemberDTO;
 import kr.co.lotteon.entity.member.Member;
+import kr.co.lotteon.mapper.MemberMapper;
 import kr.co.lotteon.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +27,8 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
     private final ModelMapper modelMapper;
     private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
+    private final JavaMailSender javaMailSender;
     
     // 회원 가입 - DB 입력
     public void save(MemberDTO memberDTO){
@@ -25,5 +36,42 @@ public class MemberService {
         memberDTO.setPass1(passwordEncoder.encode(memberDTO.getPass1())); 
         Member member = modelMapper.map(memberDTO, Member.class);
         memberRepository.save(member); 
+    }
+
+    public int selectCountUser(String Datatype, String value) {
+        return memberMapper.selectCountUser(Datatype, value);
+    }
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public void sendEmailCode(HttpSession session, String receiver){
+
+        log.info("sender : " + sender);
+
+        // MimeMessage 생성
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        // 인증코드 생성 후 세션 저장
+        int code = ThreadLocalRandom.current().nextInt(100000, 1000000);
+        session.setAttribute("code", String.valueOf(code));
+
+        log.info("code : " + code);
+
+        String title = "lotteon 인증코드 입니다.";
+        String content = "<h1>인증코드는 " + code + "입니다.</h1>";
+
+        try {
+            message.setFrom(new InternetAddress(sender, "보내는 사람", "UTF-8"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+            message.setSubject(title);
+            message.setContent(content, "text/html;charset=UTF-8");
+
+            javaMailSender.send(message);
+
+        }catch(Exception e){
+            log.error("sendEmailConde : " + e.getMessage());
+        }
+
     }
 }
