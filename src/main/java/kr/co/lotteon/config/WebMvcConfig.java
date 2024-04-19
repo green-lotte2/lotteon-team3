90ea7620788074e92296b23c5fee612eb58896dc
sun-gwang <kkj80911@gmail.com>
2024-04-19 09:20:33 +0900

    배포용

--- src/main/java/kr/co/lotteon/config/WebMvcConfig.java
+++ src/main/java/kr/co/lotteon/config/WebMvcConfig.java
@@ -1,10 +1,8 @@
 package kr.co.lotteon.config;
 
 import kr.co.lotteon.interceptor.AppInfoInterceptor;
-import kr.co.lotteon.interceptor.LocalPathInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cglib.core.Local;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -17,18 +15,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private AppInfo appInfo;
     
     // ====== 배포시엔 해당 어노테이션 사용 ======
-
-    //@Value("${myServer.static-resources-path}")
-
+    @Value("${myServer.static-resources-path}")
     // 경로 :/home/lotteon/prodImg
     
     // ====== 개발시엔 해당 어노테이션 사용 ======
-    @Value("${local.static-resources-path}")
+    //@Value("${local.static-resources-path}")
     // 경로 :prod/**
     private String staticServerPath;
 
-    @Value("${local.static-resources-path}")
-    private String localPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -39,7 +33,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AppInfoInterceptor(appInfo));
-        registry.addInterceptor(new LocalPathInterceptor(localPath));
     }
 
 }
