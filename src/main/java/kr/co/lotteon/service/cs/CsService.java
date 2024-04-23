921357bd93d29b0119b5725ca1cad62d84b2982b
123rhkddms2 <123rhkddms2@gmail.com>
2024-04-23 17:31:15 +0900

    kwangeun/fileDTO

--- src/main/java/kr/co/lotteon/service/cs/CsService.java
+++ src/main/java/kr/co/lotteon/service/cs/CsService.java
@@ -29,7 +29,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
