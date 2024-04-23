edfd5d4c796726072c21d10d9ebba8ab18b75f39
heega <m_mny1004@naver.com>
2024-04-23 10:48:24 +0900

    인덱스 인가설정 및 로그아웃

--- src/main/java/kr/co/lotteon/repository/member/MemberRepository.java
+++ src/main/java/kr/co/lotteon/repository/member/MemberRepository.java
@@ -4,8 +4,10 @@ import kr.co.lotteon.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member,String> {
-
+    Optional<Member> findByEmail(String email); // 중복 가입 확인
 
 }
