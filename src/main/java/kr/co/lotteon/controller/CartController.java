495705f2cc7fc9aa85d10845388ca553d399a58a
sun-gwang <142211574+sun-gwang@users.noreply.github.com>
2024-04-25 02:04:53 +0900

    장바구니에 추가

--- src/main/java/kr/co/lotteon/controller/CartController.java
+++ src/main/java/kr/co/lotteon/controller/CartController.java
@@ -4,7 +4,7 @@ import kr.co.lotteon.dto.product.CartDTO;
 import kr.co.lotteon.service.product.CartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-public class CartContoller {
+public class CartController {
 
     private final CartService cartService;
 
-    @PostMapping("/product/cart")
-    public String insertCart(@RequestBody CartDTO cartDTO){
-        String uid = cartDTO.getUid();
-        int ProdNo = cartDTO.getProdNo();
-        int count = cartDTO.getCount();
+    @PostMapping("/cart/insert")
+    public ResponseEntity<CartDTO> insertCart(@RequestBody CartDTO cartDTO){
 
-        return "/product/cart";
+        log.info("CartController" + cartDTO);
+        cartService.insertCart(cartDTO);
+
+        return ResponseEntity.ok(cartDTO);
     }
 }
--- src/main/java/kr/co/lotteon/service/product/CartService.java
+++ src/main/java/kr/co/lotteon/service/product/CartService.java
@@ -1,32 +1,44 @@
 package kr.co.lotteon.service.product;
 
-import groovy.util.logging.Slf4j;
 import kr.co.lotteon.dto.product.CartDTO;
 import kr.co.lotteon.entity.product.Cart;
 import kr.co.lotteon.repository.product.CartRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
+@Service @Slf4j @RequiredArgsConstructor
 public class CartService {
     private final CartRepository cartRepository;
     // 카트 넣기
     public void insertCart(CartDTO cartDTO){
-        List<Cart> onCartItem = cartRepository.findCartByUidAndProdNo(cartDTO.getUid(), cartDTO.getProdNo());
+        // 장바구니에 해당 상품이 있는지 확인
+        List<Cart> onCartItems = cartRepository.findCartByUidAndProdNo(cartDTO.getUid(), cartDTO.getProdNo());
 
-        if(onCartItem.isEmpty()){
+        // 장바구니에 상품이 없으면 추가
+        if(onCartItems.isEmpty()){
+            log.info("cartService 1" + cartDTO);
 
            Cart cart = new Cart();
            cart.setProdNo(cartDTO.getProdNo());
            cart.setUid(cartDTO.getUid());
            cart.setCount(cartDTO.getCount());
+           cart.setOpNo(cartDTO.getOpNo());
            cartRepository.save(cart);
+        }else {
+            log.info("cartService 2" + cartDTO);
+            // 해당 상품이 장바구니에 있으면 수량 추가
+            for(Cart onCartItem : onCartItems){
+                int newCount = onCartItem.getCount() + cartDTO.getCount();
+                onCartItem.setCount(newCount);
+                cartRepository.save(onCartItem);
+                log.info("cartService 3" + onCartItem);
+
+                break;
+            }
+
         }
 
     }
