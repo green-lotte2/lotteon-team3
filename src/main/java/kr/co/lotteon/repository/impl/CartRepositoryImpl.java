8421cdb97f45464c90d85883a406653092ce7536
sun-gwang <142211574+sun-gwang@users.noreply.github.com>
2024-04-30 00:51:03 +0900

    cart 내용 출력중

--- src/main/java/kr/co/lotteon/repository/impl/CartRepositoryImpl.java
+++ src/main/java/kr/co/lotteon/repository/impl/CartRepositoryImpl.java
@@ -3,6 +3,9 @@ package kr.co.lotteon.repository.impl;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import kr.co.lotteon.dto.product.CartInfoDTO;
+import kr.co.lotteon.dto.product.ProductDTO;
 import kr.co.lotteon.entity.product.QCart;
 import kr.co.lotteon.entity.product.QOption;
 import kr.co.lotteon.entity.product.QProduct;
@@ -21,25 +24,45 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CartRepositoryImpl implements CartRepositoryCustom {
 
+    private final EntityManager entityManager;
     private final JPAQueryFactory jpaQueryFactory;
     private final ModelMapper modelMapper;
-    private final QCart qCart = QCart.cart;
     private final QProduct qProduct = QProduct.product;
-    private final QOption qOption = QOption.option;
-
-
+    private final QCart qCart = QCart.cart;
     // 장바구니에 담긴 상품들 회사만 뽑아오기
     @Override
     public List<String> selectCartCompany(String uid) {
-        // SELECT GROUP_CONCAT(DISTINCT b.company) FROM product_cart AS a JOIN product AS b ON a.prodNo = b.prodNo GROUP BY b.company
-       List<Tuple> result = jpaQueryFactory
-               .select(Expressions.stringTemplate("GROUP_CONCAT({0})"),qProduct.company)
-               .from(qCart)
-               .join(qProduct).on(qCart.prodNo.eq(qProduct.prodNo))
-               .groupBy(qProduct.company)
-               .fetch();
 
-       return result.stream()
-               .map(t -> t.get(qProduct.company)).collect(Collectors.toList());
+        // SELECT GROUP_CONCAT(DISTINCT b.company) FROM product_cart AS a JOIN product AS b ON a.prodNo = b.prodNo GROUP BY b.company
+       List<String> result = entityManager.createNativeQuery(
+               "SELECT GROUP_CONCAT(DISTINCT p.company) " +
+               "FROM product_cart AS c JOIN product AS p ON c.prodNo = p.prodNo " +
+               "WHERE c.uid = :uid " +
+               "GROUP BY p.company", String.class)
+               .setParameter("uid", uid)
+               .getResultList();
+
+       log.info("cartImpl " + result);
+
+       return result;
+    }
+
+    @Override
+    public List<CartInfoDTO> selectCartProduct(String uid) {
+
+        List<Tuple> result = jpaQueryFactory.select(qCart.count, qCart.opNo, qProduct)
+                        .from(qCart)
+                        .join(qProduct).on(qCart.prodNo.eq(qProduct.prodNo))
+                        .where(qCart.uid.eq(uid))
+                        .fetch();
+        log.info("CartImpl {}",result);
+
+        List<CartInfoDTO> resultValue = result.stream()
+                                                .map(cart -> modelMapper.map(cart.toArray(), CartInfoDTO.class))
+                                                .collect(Collectors.toList());
+        log.info("CartImpl2 {}", resultValue);
+
+        return resultValue;
+
     }
 }
