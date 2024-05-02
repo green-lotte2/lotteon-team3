91b0e1418700aca1ba367081058e030f582703a3
sun-gwang <kkj80911@gmail.com>
2024-05-02 11:20:49 +0900

    article

---
+++ src/main/java/kr/co/lotteon/entity/admin/Article.java
@@ -0,0 +1,28 @@
+package kr.co.lotteon.entity.admin;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="article")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ano;
+    private String title;
+    private String content;
+    private String cate1;
+    private String cate2;
+    private String thumb;
+
+    private LocalDateTime rdate;
+}
